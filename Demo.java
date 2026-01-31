6c192e80d3001ac738d03db427dec3dfea4a9548
hemasree <hemasree2827@gmail.com>
2026-01-31 14:12:33 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,8 @@
+public class Demo {
+     public static void main(String[] args) {
+         System.out.println("hello");
+         System.out.println("world");
+         System.out.print("hello");
+         System.out.print("welcome");
+     }
+}
