16ce12e98b581030a1bf2adc3c47445dab585e12
hemasree <hemasree2827@gmail.com>
2026-01-31 16:04:51 +0530

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Data{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str1=sc.next();
+        int age=sc.nextInt();
+        double a= sc.nextDouble();
+        float b=sc.nextFloat();
+        System.out.println(str1);
+        System.out.println(age);
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
